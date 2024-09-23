dd1cab8dbd4897fbe0940162bd39fc3dd2d76c4c
luis.polanco-at-050384910613 <lpolanco@mycontrolgps.com>
2024-09-23 14:34:37 -0600

    fix

--- app/admin/orders/api/route.ts
+++ app/admin/orders/api/route.ts
@@ -1,6 +1,10 @@
 import { prisma } from "@/src/lib/prisma"
 
+
+export const dynamic = 'force-dynamic'
+
 export async function GET() {
+
 
     const orders = await prisma.order.findMany({
         where: {
--- app/orders/api/route.tsx
+++ app/orders/api/route.tsx
@@ -1,6 +1,9 @@
 import { prisma } from "@/src/lib/prisma";
 
 
+export const dynamic = 'force-dynamic'
+
+
 export async function GET() {
     const orders = await prisma.order.findMany({
         take: 5,
